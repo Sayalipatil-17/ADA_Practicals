6f118d06d4ca87c7cc722321e8dcac63733035c5
Sayalipatil-17 <sayalipatil1772005@gmail.com>
2025-10-29 09:28:18 +0530

    Replace linear search with quicksort implementation

--- 2.2_quicksort.c
+++ 2.2_quicksort.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int LinearSearch(int Array[], int n, int key) {
+void generateRandomArray(int arr[], int n) {
 for (int i = 0; i < n; i++) {
-if (Array[i] == key) {
-return i;
+arr[i] = rand() % 100;
 }
 }
-return -1;
+int partition(int arr[], int low, int high) {
+int pivot = arr[high];
+int i = low - 1;
+
+for (int j = low; j < high; j++) {
+if (arr[j] <= pivot) {
+i++;
+int temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
 }
-int main() {
-int n, key, result;
-clock_t start, stop;
-double time_taken;
-printf("Enter the size of the array: ");
-if (scanf("%d", &n) != 1 || n <= 0) {
-printf("Invalid input. Please enter a positive integer.\n");
-return 1;
-}
-int *Array = (int *)malloc(n * sizeof(int));
-if (Array == NULL) {
-printf("Memory allocation failed.\n");
-return 1;
-}
-for (int i = 0; i < n; i++) {
-Array[i] = i + 1;
 }
 
-key = n;
+int temp = arr[i + 1];
+arr[i + 1] = arr[high];
+arr[high] = temp;
+return i + 1;
+}
+void quickSort(int arr[], int low, int high) {
+if (low < high) {
+int pi = partition(arr, low, high);
+quickSort(arr, low, pi - 1);
+quickSort(arr, pi + 1, high);
+}
+}
+int main() {
+int n;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+int *arr = (int *)malloc(n * sizeof(int));
+if (arr == NULL) {
+printf("Memory allocation failed\n");
+return 1;
+}
 srand(time(NULL));
-start = clock();
+clock_t start = clock();
 for (int i = 0; i < 1000; i++) {
-result = LinearSearch(Array, n, key);
+generateRandomArray(arr, n);
+quickSort(arr, 0, n - 1);
 }
-stop = clock();
-time_taken = ((double)(stop - start)) / CLOCKS_PER_SEC;
-if (result != -1) {
-printf("Element %d found at index %d.\n", key, result);
-} else {
-printf("Element %d not found.\n", key);
-}
-printf("Time taken for 1000 searches: %.6f seconds\n", time_taken);
-free(Array);
+clock_t end = clock();
+double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC / 1000.0;
+printf("Average time to sort %d elements using Quick Sort: %f seconds\n", n,
+time_taken);
+free(arr);
 return 0;
 }
