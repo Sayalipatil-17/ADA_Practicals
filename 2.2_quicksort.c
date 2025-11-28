d0dd485fa03a7582a0eba04da42f0832b437fcbf
Sayalipatil-17 <sayalipatil1772005@gmail.com>
2025-11-28 07:10:00 +0530

    Refactor QuickSort and add print functionality
    Refactor QuickSort implementation and add array printing.

--- 2.2_quicksort.c
+++ 2.2_quicksort.c
@@ -1,55 +1,103 @@
+//quick sort
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-void generateRandomArray(int arr[], int n) {
-for (int i = 0; i < n; i++) {
-arr[i] = rand() % 100;
-}
-}
-int partition(int arr[], int low, int high) {
-int pivot = arr[high];
-int i = low - 1;
+#include<time.h>
 
-for (int j = low; j < high; j++) {
-if (arr[j] <= pivot) {
-i++;
-int temp = arr[i];
-arr[i] = arr[j];
-arr[j] = temp;
-}
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
-int temp = arr[i + 1];
-arr[i + 1] = arr[high];
-arr[high] = temp;
-return i + 1;
+// Partition function
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = (low + 1);
+    int j = high;
+
+        do {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+    
+            while (arr[j] > pivot){
+                j--;
+            }
+    
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        } while (i < j);
+    
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+    
+        return j;
+    }
+
+
+// QuickSort function
+void quickSort(int arr[], int low, int high){
+    if (low < high){
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
-void quickSort(int arr[], int low, int high) {
-if (low < high) {
-int pi = partition(arr, low, high);
-quickSort(arr, low, pi - 1);
-quickSort(arr, pi + 1, high);
+
+void generateRandomArray(int arr[],int size){
+    for(int i=0; i<size; i++){
+        arr[i]=rand()%100;
+    }
+}
+
+
+// Main function to test the QuickSort implementation
+int main(){
+    int n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+
+    int *arr=(int*)malloc(n*sizeof(int));
+    int *temp=(int*)malloc(n*sizeof(int));
+    if (arr == NULL || temp == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    generateRandomArray(arr, n);
+
+    double total_time = 0;
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[j] = arr[j];
+
+        clock_t start = clock();
+        quickSort(arr, 0, n - 1);
+        clock_t end = clock();
+
+        total_time += ((double)(end - start)) / CLOCKS_PER_SEC;
+    }
+
+    printf("Average time to sort %d elements over 1000 runs: %f seconds\n", n, total_time / 1000.0);
+
+    free(arr);
+    free(temp);
+
+    return 0;
+    printArray(arr, n);
+    quickSort(arr, 0, n - 1);
+    printArray(arr, n);
+    return 0;
 }
 }
-int main() {
-int n;
-printf("Enter the number of elements: ");
-scanf("%d", &n);
-int *arr = (int *)malloc(n * sizeof(int));
-if (arr == NULL) {
-printf("Memory allocation failed\n");
-return 1;
-}
-srand(time(NULL));
-clock_t start = clock();
-for (int i = 0; i < 1000; i++) {
-generateRandomArray(arr, n);
-quickSort(arr, 0, n - 1);
-}
-clock_t end = clock();
-double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC / 1000.0;
-printf("Average time to sort %d elements using Quick Sort: %f seconds\n", n,
-time_taken);
-free(arr);
-return 0;
-}
+
